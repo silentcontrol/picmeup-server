3d03148db543987b7b19c97ea3f834c9b48a4e17
Chris Wang <chriswang@Chriss-MacBook-Pro-3.local>
2018-10-02 15:17:16 -0400

    updated db helper and index route

--- db/insertHelper.js
+++ db/insertHelper.js
@@ -9,7 +9,7 @@ module.exports = function (knex) {
               product_name: productName,
               price_in_cents: priceInCents
             }
-          ])
+          ]).returning('*')
     },
     // users table helper
     addUser: async (firstName, lastName, email, password) => {
